7b70c2905b1381f36b0a074957b9c826340c638e
李二狗 <appid@zy5d.com>
2023-05-03 18:50:27 +0800

    修正监控地址

--- Templates/Baota.php
+++ Templates/Baota.php
@@ -16,7 +16,7 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 step=20
 for (( i = 0; i < 60; i=(i+step) )); do
-curl -sS --connect-timeout 10 -m 60 'https://<?php echo $_SERVER['SERVER_NAME'];?>/bapi/Mapi/notify_pay'
+curl -sS --connect-timeout 10 -m 60 'https://<?php echo $_SERVER['SERVER_NAME'];?>/index.php/Api/pay'
 echo "----------------------------------------------------------------------------"
 endDate=`date +"%Y-%m-%d %H:%M:%S"`
 echo "★[$endDate] Successful"
